1b040f65dc53461d8875692786f639b85ece9d21
Rolf Petersen <rollep7@msn.com>
2016-11-11 11:54:28 +0100

    Blend og databind

---
+++ demo/viewmodel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace demo.viewmodel
+{
+    
+    class MainViewModel : INotifyPropertyChanged
+    {
+        //attribut
+        private string sometext;
+
+        //property
+        public string Sometext
+        {
+            get { return sometext;}
+            set
+            {
+                sometext = value;
+                NotifyView("Sometext");
+            }
+        }
+
+        // dette er en konstruktør til at initialiserer alle attributter/instans variable.
+        public MainViewModel()
+        {
+            Sometext = "Load værdi";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyView(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this,new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
